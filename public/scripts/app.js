ea58cecaa54c809324560e3065783820ae5f9141
kevinmacarthur <kmacarthur16@gmail.com>
2018-10-20 20:57:11 +0000

    updated template literal to include likes and ratings

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -216,21 +216,32 @@ function renderResources(resources) {
 // Function to add the attributes for each tweet to create a dynamic HTML page.
 function createResourceElement(resource) {
    let newScript = `
-        <div class="col-md-4">
-           <div class="card mb-4 shadow-sm">
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src=${resource.image_url} data-holder-rendered="true">
+            <div class="col-xl-4">
+             <div class="card sm-2 shadow-sm">
+                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 50%; width: 100%; display: block;" src=${resource.image_url} data-holder-rendered="true">
                 <div class="card-body">
                   <p class="card-text">${resource.title}</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                       <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary"><a target="_blank" rel="noopener noreferrer" href="${resource.resource_url}"> Visit</a></button>
+                      <button type="button" class="btn btn-sm btn-outline-secondary"><a target="_blank" rel="noopener noreferrer" href="${resource.resource_url}">Visit</a></button>
+                      <button type="button" class="btn btn-sm btn-outline-secondary buttonLike">Like</button>
+                      <div class="dropdown">
+                      <button type="button" class="btn btn-sm btn-outline-secondary buttonRate">Rate</button>
+                        <div class="dropdown-content">
+                          <form class="newRatingForm" >
+                            <textarea class="UserRate" name="UserRate"></textarea>
+                            <input class="postRating" type="submit" value="Rate!">
+                          </form>
+                        </div>
+                      </div>
                     </div>
-                    <small class="text-muted">9 mins</small>
                   </div>
+                  <small class="textMuted"> Likes: 9 </small>
+                  <small class="textMuted"> Avg Rating: 10 </small>
                 </div>
-            </div>
-        </div>`
+              </div>
+            </div>`
 
   return newScript;
 
