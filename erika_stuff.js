f22cb857427150726dfc7f3abe8eeacd123fb1bc
Erika Szabo <erika.szabo.geo@gmail.com>
2018-10-21 14:46:25 +0000

    temp save

---
+++ erika_stuff.js
@@ -0,0 +1,20 @@
+router.post("/update", (req, res) => {
+
+  let password = req.body.newpassword;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
+   if (!password) {       //Checks to see if password is empty
+      console.log("Please insert password!");
+    } else {
+     knex("users")
+      .select("*")
+      .where("token", req.params.token)
+      .then((user) => {
+          knex("users")
+            .update("user.password", hashedPassword);
+        }
+      .catch(e => {
+        console.log("Oops something went wrong", e);
+      })
+    }
+  })
