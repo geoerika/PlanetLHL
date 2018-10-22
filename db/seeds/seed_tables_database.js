4f94f7a35d20beab9248444276b976fc7e40a424
Erika Szabo <erika.szabo.geo@gmail.com>
2018-10-22 00:37:02 +0000

    changed initial seeding file

--- db/seeds/seed_tables_database.js
+++ db/seeds/seed_tables_database.js
@@ -12,56 +12,17 @@ exports.seed = async function(knex, Promise) {
         knex('users').insert({name: 'Aaron', password: 'aaron' }).returning("id")
       ]);
 
-      const res_id = [];
       const resources = await Promise.all([
         // Inserts seed entries in resources
-      knex('resources').insert({title: 'Mount St. Helens', description: 'video of eruption in 1980',
+        knex('resources').insert({title: 'Mount St. Helens', description: 'video of eruption in 1980',
                                           resource_url: 'https://video.nationalgeographic.com/wild/disaster-at-mount-st-helens',
-                                          created_at: 10, rating: 5, likes: 3, users_id: users[0][0]}).returning('id')
-        .then((response) => {
-          res_id.push(response[0]);
-          knex('resources').insert({title: 'The Moose', description: 'video of caribou migration',
+                                          created_at: 10, rating: 0, likes: 0, users_id: users[0][0], image_url:'http://pmdvod.nationalgeographic.com/NG_Video/112/771/239818307887_1080i_2997_DisasterAtMountStHelens_DMS_640x360_249225795661.jpg'}),
+        knex('resources').insert({title: 'The Caribou migration', description: 'video of caribou migration',
                                           resource_url: 'https://www.youtube.com/watch?v=r6jcqCm5C1Q&t=29s',
-                                          created_at: 5, rating: 5, likes: 2, users_id: users[1][0]}).returning('id')
-                .then((response) => {
-                  res_id.push(response[0]);
-                  knex('resources').insert({title: 'Pumpkins', description: 'the secret lives of pumpkins',
-                                                  resource_url: 'http://www.thepumpkingeek.com/id6.html',
-                                                  created_at: 2, rating: 5, likes: 5, users_id: users[2][0]}).returning('id')
-                  .then((response) => { return res_id.push(response[0])})
-                })
-        })
-
-
-      ]);
-
-      const tags = await Promise.all([
-        // Inserts seed entries tags
-        knex('tags').insert({name: 'volcano'}).returning('id'),
-        knex('tags').insert({name: 'caribou'}).returning('id'),
-        knex('tags').insert({name: 'pumpkin'}).returning('id'),
-        knex('tags').insert({name: 'geology'}).returning('id'),
-        knex('tags').insert({name: 'animal'}).returning('id'),
-        knex('tags').insert({name: 'plant'}).returning('id')
-      ]);
-
-      const resource_tags = await Promise.all([
-        // Inserts seed entries in resource_tags
-        knex('resource_tags').insert({resources_id:res_id[0], tags_id: tags[0][0]})
-        .then (() => {
-            knex('resource_tags').insert({resources_id:res_id[1], tags_id: tags[4][0]})
-            .then ( () => {
-                knex('resource_tags').insert({resources_id:res_id[2], tags_id: tags[5][0]})
-                .then (() => {})
-            })
-        })
-      ]);
-
-      const comments = await Promise.all([
-        // Inserts seed entries in comments
-        knex('comments').insert({comment: 'Absolutely amazing!', users_id:users[1][0], resources_id:res_id[0]}),
-        knex('comments').insert({comment: 'WOW', users_id:users[0][0], resources_id:res_id[1]}),
-        knex('comments').insert({comment: 'SPOOOOKY', users_id:users[2][0], resources_id:res_id[2]}),
+                                          created_at: 5, rating: 0, likes: 0, users_id: users[1][0], image_url:'https://i.ytimg.com/vi/r6jcqCm5C1Q/maxresdefault.jpg'}),
+        knex('resources').insert({title: 'Pumpkins', description: 'the secret lives of pumpkins',
+                                          resource_url: 'http://www.thepumpkingeek.com/id6.html',
+                                          created_at: 2, rating: 0, likes: 0, users_id: users[2][0], image_url:'http://www.thepumpkingeek.com/sitebuildercontent/sitebuilderpictures/.pond/Vader10.jpg.w300h308.jpg'}),
       ]);
     });
 };
